de432cbceba97e4080ec0b567915efc6ed4e4f0b
Khoroshikh Arkadii <a.khoroshikh@cs.money>
2022-09-19 16:50:53 +0300

    Cards. Append images.

--- src/screens/CardsScreen/CardsScreen.tsx
+++ src/screens/CardsScreen/CardsScreen.tsx
@@ -1,6 +1,25 @@
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import React from 'react';
 
+const CARDS = {
+  1: {
+    image: require('src/assets/images/1.jpg'),
+    size: { width: 450, height: 589 },
+  },
+  2: {
+    image: require('src/assets/images/2.jpg'),
+    size: { width: 450, height: 529 },
+  },
+  3: {
+    image: require('src/assets/images/3.jpg'),
+    size: { width: 450, height: 566 },
+  },
+  4: {
+    image: require('src/assets/images/4.jpg'),
+    size: { width: 450, height: 573 },
+  },
+};
+
 const CardsScreen = () => {
   return (
     <SafeAreaView>
